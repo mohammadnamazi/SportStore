7efc4fb99b8fa69ef8b67b621bfa8580b27f2f74
Mohammad Namazi <37037287+mohammadnamazi@users.noreply.github.com>
2021-11-30 00:23:41 +0330

    SetingUpDatabase And Migration

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -5,8 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SportsStore.Models;
 
 namespace SportsStore
 {
@@ -14,9 +17,20 @@ namespace SportsStore
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<StoreDbContext>(option =>
+            {
+                option.UseSqlServer(Configuration["ConnectionStrings:SportsStoreConnection"]);
+            });
+            services.AddScoped<IStoreRepository, EFStoreRepository>(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +48,8 @@ namespace SportsStore
             {
                 endpoints.MapDefaultControllerRoute();
             });
+
+            SeedData.EnsurePopulated(app);
         }
     }
 }
